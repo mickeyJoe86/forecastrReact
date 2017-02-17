26917a3fc9fda6f555ebe3c8fcd98088be68dbb1
Mike Ward <ward.michael51@gmail.com>
2017-02-17 15:35:44 -0500

    Added mock data for iteration

---
+++ src/MockData.js
@@ -0,0 +1,15 @@
+var MockData = function () {
+    return [
+        {
+            id: 1,
+            title: "Test",
+            body: "Anim pariatur cliche reprehenderit, enim eiusmod high life"
+        },
+        {
+            id: 2,
+            title: "Test 2",
+            body: "Anim pariatur cliche reprehenderit, enim eiusmod high life"
+        }
+    ];
+}
+export default MockData;
--- src/components/SevenDay.js
+++ src/components/SevenDay.js
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Accordion, Panel, Glyphicon } from 'react-bootstrap';
+import MockData from '../MockData';
 
-const title = (
-    <div>
-        <Glyphicon glyph="glyphicon glyphicon-plus-sign" />
-        <span> Test</span>
-    </div>
-)
-
-class SevenDay extends Component {
-    render() {
+var SevenDay = React.createClass({
+    getInitialState: function () {
+        return { panels: [] }
+    },
+    componentDidMount: function () {
+        this.setState({            
+            panels: MockData()
+        });
+    },
+    render: function () {
         return (
             <div>
                 <h2>Weekly Forecast</h2>
                 <hr />
                 <Accordion>
-                    <Panel header={ title } eventKey="1">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                    </Panel>
-                    <Panel header={ title } eventKey="2">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                    </Panel>
-                    <Panel header={ title } eventKey="3">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                    </Panel>
-                </Accordion>            
+                    {this.state.panels.map(function (panel, index) {
+                        return (
+                            <Panel key={panel.id}
+                                header={
+                                    <div>
+                                        <Glyphicon glyph="glyphicon glyphicon-plus-sign" />
+                                        <span> {panel.title}</span>
+                                    </div>
+                                }
+                                eventKey={panel.id}>
+                                {panel.body}
+                            </Panel>
+                        )
+                    })}
+                </Accordion>
             </div>
-
         );
     }
-}
+});
 
 export default SevenDay;
